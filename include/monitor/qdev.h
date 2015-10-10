642de7e6ac11ae8e354ed38715983cdc98a91360
Michael Davidsaver <mdavidsaver@gmail.com>
2015-10-10 14:05:30 -0400

    add shell "info gpios"
    Walks the device tree and shows information on GPIO (aka irq)
    connections.

--- include/monitor/qdev.h
+++ include/monitor/qdev.h
@@ -8,6 +8,7 @@
 void hmp_info_qtree(Monitor *mon, const QDict *qdict);
 void hmp_info_qdm(Monitor *mon, const QDict *qdict);
 void hmp_info_qom_tree(Monitor *mon, const QDict *dict);
+void hmp_info_gpio(Monitor *mon, const QDict *qdict);
 void qmp_device_add(QDict *qdict, QObject **ret_data, Error **errp);
 
 int qdev_device_help(QemuOpts *opts);
