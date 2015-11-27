77ddb5bea8de27ab764661afdb966c3052e3d22f
Michael Davidsaver <mdavidsaver@gmail.com>
2015-11-27 14:13:44 -0500

    armv7m: priority field mask
    Many v7m CPUs don't implement all of the 8 bits
    of the priority fields.  Typically, only the
    top N bits are available.  Existing practice
    implies that writes to unimplemented
    bits will be ignore, and read as zero.
    
    This allows a guest to discover the implemented
    bits by writing 0xff to (eg. basepri).
    The value read back will have only the available
    bits set.

--- target-arm/cpu-qom.h
+++ target-arm/cpu-qom.h
@@ -111,6 +111,12 @@ typedef struct ARMCPU {
     /* PMSAv7 MPU number of supported regions */
     uint32_t pmsav7_dregion;
 
+    /* Some v7-M targets don't impliment the full 8 bits
+     * of the priority fields.  Writes to unimplimented
+     * bits are treated as zero (guest can discover mask).
+     */
+    uint8_t v7m_priority_mask;
+
     /* PSCI conduit used to invoke PSCI methods
      * 0 - disabled, 1 - smc, 2 - hvc
      */
