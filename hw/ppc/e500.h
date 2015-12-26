8c721c27c188430d4963cace43eb718792e94b40
Michael Davidsaver <mdavidsaver@gmail.com>
2015-12-26 10:05:53 -0500

    e500: add more board config options
    allow board code to skip common NIC and guest image setup
    and configure decrementor frequency.
    Existing boards unchanged.

--- hw/ppc/e500.h
+++ hw/ppc/e500.h
@@ -22,6 +22,9 @@ typedef struct PPCE500Params {
     hwaddr pci_mmio_base;
     hwaddr pci_mmio_bus_base;
     hwaddr spin_base;
+    uint32_t decrementor_freq; /* in Hz */
+    bool skip_load;
+    bool tsec_nic;
 } PPCE500Params;
 
 void ppce500_init(MachineState *machine, PPCE500Params *params);
