c78a305e1328b5c290c85ae70dea830e0d53a700
Michael Davidsaver <mdavidsaver@gmail.com>
2017-06-10 21:01:23 +0200

    add mvme3100-test
    Exercise some features of the mvme3100 CPLD logic
    and read from the eeprom w/ VPD.
    
    Signed-off-by: Michael Davidsaver <mdavidsaver@gmail.com>

---
+++ tests/mvme3100-test.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+
+#include "qemu/osdep.h"
+#include "libqtest.h"
+#include "libqos/libqos.h"
+#include "libqos/i2c.h"
+
+#define assert_equal(A, B) g_assert_cmphex((A), ==, (B))
+
+static
+I2CAdapter *i2c;
+
+static
+void test_ccsr(void)
+{
+    /* CCSRBAR is self referential */
+    assert_equal(readl(0xff700000), 0x000ff700);
+
+    /* introspect memory size */
+    assert_equal(readl(0xff702080), 0x80000000);
+    /* value is (ram_size-1)>>24 */
+    assert_equal(readl(0xff702000), 15);
+}
+
+static
+void test_cpld(void)
+{
+    /* read/write to test register */
+    assert_equal(readl(0xe2000010), 0x00000000);
+    assert_equal(readl(0xe2000014), 0xffffffff);
+
+    writel(0xe2000010, 0x12345678);
+
+    assert_equal(readl(0xe2000010), 0x12345678);
+    assert_equal(readl(0xe2000014), 0x12345678 ^ 0xffffffff);
+}
+
+static
+void test_eeprom(void)
+{
+    char buf[16];
+
+    i2c_recv(i2c, 0xa8, (uint8_t *)buf, 8);
+    buf[8] = '\0';
+
+    /* Read header for Motorola VPD info */
+    g_assert_cmpstr(buf, ==, "MOTOROLA");
+}
+
+int main(int argc, char *argv[])
+{
+    int ret;
+    g_test_init(&argc, &argv, NULL);
+
+    qtest_start("-machine mvme3100-1152");
+
+    i2c = e500_i2c_create(0xff700000);
+
+    qtest_add_func("/mvme3100/ccsr", test_ccsr);
+    qtest_add_func("/mvme3100/cpld", test_cpld);
+    qtest_add_func("/mvme3100/eeprom", test_eeprom);
+
+    ret = g_test_run();
+
+    printf("Tests done\n");
+
+    qtest_end();
+    printf("Tests end\n");
+
+    return ret;
+}
